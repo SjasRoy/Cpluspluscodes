aa7bad77f853abc9090334754da006acf8abcbe0
SjasRoy <droy8195@gmail.com>
2026-08-26 19:34:22 +0530

    Create palindromecheck.cpp

---
+++ palindromecheck.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char string[20];
+    int i, length;
+    int flag = 0;
+
+    cout << "Enter a string: ";
+    cin >> string;
+
+    length = strlen(string);
+
+    for (i = 0; i < length; i++)
+    {
+        if (string[i] != string[length - i - 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << string << " is not a palindrome";
+    else
+        cout << string << " is a palindrome";
+
+    return 0;
+}
+
+OUTPUT 
+Enter a string: madam
+madam is a palindrome
